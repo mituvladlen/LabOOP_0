42044469ba79ca73e42b4e5a27db87a8a057afe0
Vladlen Mitu <vladlen.mitu@gmail.com>
2024-12-04 10:24:26 +0200

    Task 5

---
+++ lab-papers-please/java-classifcation/src/main/java/oop/Lab3/Main.java
@@ -0,0 +1,8 @@
+package oop.Lab3;
+
+public class Main {
+    public static void main(String[] args) {
+        ScheduledTasks tasks = new ScheduledTasks();
+        tasks.startScheduledTasks();
+    }
+}
